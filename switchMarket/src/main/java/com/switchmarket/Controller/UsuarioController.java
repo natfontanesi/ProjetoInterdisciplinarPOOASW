191cbf6227d75c56c75ce332c49b958d95baef66
natfontanesi <natfontanesi@gmail.com>
2021-11-25 22:21:04 -0300

    atualizando

--- switchMarket/src/main/java/com/switchmarket/Controller/UsuarioController.java
+++ switchMarket/src/main/java/com/switchmarket/Controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.switchmarket.Controller;
 
+import com.switchmarket.DTO.RequisicaoNovoUsuario;
 import com.switchmarket.Entities.Usuario;
 import com.switchmarket.Enums.estadoEnum;
 import com.switchmarket.Repositories.UsuarioRepo;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
@@ -29,9 +31,15 @@ public class UsuarioController {
     @GetMapping("/usuarios/novo")
     public ModelAndView newUser(){
 
-        ModelAndView mv = new ModelAndView("usuario/novo");
+        ModelAndView mv = new ModelAndView("usuarios/novo");
         mv.addObject("estado", estadoEnum.values());
+
         return mv;
 
     }
+    @PostMapping("/usuarios")
+    public String create(RequisicaoNovoUsuario requisicao){
+        return "redirect:/usuarios";
+    }
+
 }
---
+++ switchMarket/src/main/java/com/switchmarket/DTO/RequisicaoNovoUsuario.java
@@ -0,0 +1,72 @@
+package com.switchmarket.DTO;
+
+import com.switchmarket.Enums.estadoEnum;
+
+public class RequisicaoNovoUsuario {
+
+    private int idUser;
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String cidade;
+    private estadoEnum estado;
+    private String cep;
+
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
